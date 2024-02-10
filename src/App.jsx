8324fa2c74fe9ce1e51841a5768c484a924409b5
mdtanvirahamedshanto <mdtanvirahamdsanto987@gmail.com>
2024-02-10 23:12:08 +0600

    Context

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import "./App.css";
 import Page from "./components/Page";
+import WeatherProvider from "./provider/WeatherProvider";
 
 export default function App() {
   return (
-    <div className="dark">
-      <Page />
-    </div>
+    <WeatherProvider>
+      <div className="dark">
+        <Page />
+      </div>
+    </WeatherProvider>
   );
 }
---
+++ src/provider/WeatherProvider.jsx
@@ -0,0 +1,14 @@
+/* eslint-disable react/prop-types */
+import { WeatherContext } from "../contexts";
+import useWeather from "../hooks/useWeather";
+
+const WeatherProvider = ({ children }) => {
+  const { weatherData, loading, isError } = useWeather();
+  return (
+    <WeatherContext.Provider value={{ weatherData, loading, isError }}>
+      {children}
+    </WeatherContext.Provider>
+  );
+};
+
+export default WeatherProvider;
