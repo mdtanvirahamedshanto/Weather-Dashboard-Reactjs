33af0f544885bc269e6091486f65fa3ebdf82c6c
mdtanvirahamedshanto <mdtanvirahamdsanto987@gmail.com>
2024-02-11 09:32:51 +0600

    handle fvt button

--- src/components/Header/FavList.jsx
+++ src/components/Header/FavList.jsx
@@ -1,8 +1,12 @@
-const FavList = () => {
+/* eslint-disable react/prop-types */
+const FavList = ({ onFvt }) => {
   return (
-    <div className="p-2 hover:bg-black/30 cursor-pointer flex gap-2 items-center rounded-md transition-all">
+    <div
+      className="p-2 hover:bg-black/30 cursor-pointer flex gap-2 items-center rounded-md transition-all"
+      onClick={onFvt}
+    >
       <img src="./assets/heart.svg" alt="" />
-      <span>Favourite Locations</span>
+      <button>Favourite Locations</button>
     </div>
   );
 };
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,8 +1,14 @@
+/* eslint-disable no-unused-vars */
+import { useState } from "react";
 import FavList from "./FavList";
 import FavListModel from "./FavListModel";
 import SearchBox from "./SearchBox";
 
 const Header = () => {
+  const [isShow, setIsShow] = useState(false);
+  const handleFvt = () => {
+    setIsShow(!isShow);
+  };
   return (
     <header className="fixed w-full top-0 z-50 bg-gradient-to-b from-black/60 to-black/0 pb-10">
       <nav className="container flex items-center justify-between py-6">
@@ -11,36 +17,11 @@ const Header = () => {
         </a>
 
         <div className="flex items-center gap-4 relative">
-          {/* <form action="#">
-            <div className="flex items-center space-x-2 py-2 px-3 group focus-within:bg-black/30 transition-all border-b border-white/50 focus-within:border-b-0 focus-within:rounded-md">
-              <input
-                className="bg-transparent  placeholder:text-white text-white w-full text-xs md:text-base outline-none border-none"
-                type="search"
-                placeholder="Search Location"
-                required
-              />
-              <button type="submit">
-                <img src="./assets/search.svg" />
-              </button>
-            </div>
-          </form> */}
           <SearchBox />
-          {/* <div className="p-2 hover:bg-black/30 cursor-pointer flex gap-2 items-center rounded-md transition-all">
-            <img src="./assets/heart.svg" alt="" />
-            <span>Favourite Locations</span>
-          </div> */}
-          <FavList />
 
-          {/* <!-- Modal --> */}
-          {/* <div className="max-w-xs py-4 bg-white rounded-md border-gray-500 absolute right-0 top-16 text-black shadow-lg ">
-            <h3 className="text-lg font-bold px-4">Favourite Locations</h3>
-            <ul className="space-y-2 mt-4 *:py-2 *:px-4 *:cursor-pointer">
-              <li className="hover:bg-gray-200">Dhaka</li>
-              <li className="hover:bg-gray-200">Rangpur</li>
-              <li className="hover:bg-gray-200">Europe</li>
-            </ul>
-          </div> */}
-          <FavListModel />
+          <FavList onFvt={handleFvt} />
+
+          {isShow && <FavListModel />}
         </div>
       </nav>
     </header>
