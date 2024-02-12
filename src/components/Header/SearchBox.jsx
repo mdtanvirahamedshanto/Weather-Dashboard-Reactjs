cceed39a69be240cc4b9f362114215d368fca6bf
mdtanvirahamedshanto <mdtanvirahamdsanto987@gmail.com>
2024-02-12 12:33:04 +0600

    Debouce

--- src/components/Header/SearchBox.jsx
+++ src/components/Header/SearchBox.jsx
@@ -1,24 +1,29 @@
-import { useContext, useState } from "react";
+import { useContext } from "react";
 import { LocationContext } from "../../contexts";
 import { getLocationByName } from "../../data/location-data";
+import useDebounce from "../../hooks/useDebounce";
 
 const SearchBox = () => {
-  const [searchTerm, setSearchTerm] = useState("");
   const { setSelectedLocation } = useContext(LocationContext);
   // console.log(selectedLocation);
-  const handleSearchSubmit = (e) => {
-    e.preventDefault();
-    const fetchLocation = getLocationByName(searchTerm);
+
+  const onSearch = useDebounce((text) => {
+    const fetchLocation = getLocationByName(text);
     setSelectedLocation({ ...fetchLocation });
+  }, 500);
+
+  const handleChange = (e) => {
+    const value = e.target.value;
+    onSearch(value);
   };
   return (
-    <form onSubmit={handleSearchSubmit}>
+    <form>
       <div className="flex items-center space-x-2 py-2 px-3 group focus-within:bg-black/30 transition-all border-b border-white/50 focus-within:border-b-0 focus-within:rounded-md">
         <input
           className="bg-transparent  placeholder:text-white text-white w-full text-xs md:text-base outline-none border-none"
           type="search"
           placeholder="Search Location"
-          onChange={(e) => setSearchTerm(e.target.value)}
+          onChange={handleChange}
           required
         />
         <button type="submit">
